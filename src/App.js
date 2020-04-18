5628b1d34014062f244942d19970ddca21c2b5b1
fish <re4lawliet@gmail.com>
2020-04-17 18:51:28 -0600

    Video demos

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Carlos Monterroso 201314646</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Video de Tarea
         </p>
       </div>
     );
